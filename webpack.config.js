0f727c94a36797d09029f2ddddedc0c5ad4edf69
APG <apg.prywatny@gmail.com>
2019-11-06 14:20:57 +0100

    Add project files

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const entryPath = "app";
+const entryFile = "app.js";
+
+module.exports = {
+  entry: `./${entryPath}/js/${entryFile}`,
+  output: {
+    filename: "out.js",
+    path: path.resolve(__dirname, `${entryPath}/build`)
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `${entryPath}`),
+    publicPath: "/build/",
+    compress: true,
+    port: 3004
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [autoprefixer()]
+            }
+          },
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(jpe?g|gif|png|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          publicPath: "/images/",
+          outputPath: "/images/"
+        }
+      }
+    ]
+  }
+};
